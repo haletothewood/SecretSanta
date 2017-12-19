431b775f2ec10b3b619db8da008ac7d50a919e18
David Halewood <davidnhalewood@hotmail.com>
2017-12-19 21:52:49 +0000

    Removing debugging lines

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
   $('.btn').click(function (e) {
     var user = $("input[id=name]").val();
     ss.addUser(user)
-    console.log(ss.showList())
     e.preventDefault()
     $("input[id=name]").val("")
   });
