745b773c91dd4683b78e78a86985ec266210d436
David Halewood <davidnhalewood@hotmail.com>
2017-12-19 14:44:05 +0000

    Adding pickUser function

--- spec/secretSantaSpec.js
+++ spec/secretSantaSpec.js
@@ -1,6 +1,8 @@
 describe("Secret Santa", function () {
 
   var user = "User"
+  var user2 = "User2"
+  var user3 = "User3"
   var ss = SecretSanta()
 
   it("adds users to a list", function () {
@@ -9,10 +11,12 @@ describe("Secret Santa", function () {
   })
 
   it("can add multiple users to a list", function () {
-    var user2 = "User2"
-    var user3 = "User3"
     ss.addUser(user2)
     ss.addUser(user3)
     expect(ss.showList()).toContain(user, user2, user3)
+  })
+
+  it("picks a random user from a list", function () {
+    expect([user, user2, user3]).toContain(ss.pickUser())
   })
 })
--- src/secretSanta.js
+++ src/secretSanta.js
@@ -9,11 +9,18 @@
 
     function addUser(user) {
       list.push(user)
+      return user
+    }
+
+    function pickUser() {
+      var randomUser = list[Math.floor(Math.random() * list.length)];
+      return randomUser
     }
 
     return {
       showList: showList,
-      addUser: addUser
+      addUser: addUser,
+      pickUser: pickUser
     }
   };
 })(this);
